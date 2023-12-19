908af3bf21aacfa565010cae0bc90b233583eb20
Glynn Quelch <glynn.quelch@gmail.com>
2023-12-19 16:20:44 +0000

    WIP on block

--- blocks/src/colophon/edit.js
+++ blocks/src/colophon/edit.js
@@ -3,7 +3,12 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, PanelRow } from '@wordpress/components';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { TextControl, CheckboxControl } from '@wordpress/components';
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -22,6 +27,31 @@ import { useBlockProps } from '@wordpress/block-editor';
 import './editor.scss';
 
 /**
+ * Replace any default link texts.
+ *
+ * @since 1.0.0
+ *
+ * @param {object} attributes The block attributes.
+ *
+ * @return {object} The modified attributes.
+ */
+const replaceLinkTextPlaceHolders = (attributes) => {
+	// If pressableLink is __ or not set
+	if (attributes.pressableLink === '__' || !attributes.pressableLink ) {
+		// Set pressableLink to the value of linkText.
+		attributes.pressableLink = __('Hosted by Pressable.', 'wpcomsp-donations');
+	}
+
+	// If wpCLink is __ or not set
+	if (attributes.wpComLink === '__' || !attributes.wpComLink) {
+		// Set wpCLink to the value of linkText.
+		attributes.wpComLink = __('Proudly powered by WordPress.', 'wpcomsp-donations');
+	}
+
+	return attributes;
+}
+
+/**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
@@ -29,13 +59,88 @@ import './editor.scss';
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	attributes = replaceLinkTextPlaceHolders(attributes);
+
+	console.log(attributes);
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Todo List – hello from the editor!',
-				'wpcomsp-scaffold'
-			) }
-		</p>
+
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={__('Settings', 'textdomain')}>
+					<PanelRow>
+						<p>HI</p>
+					</PanelRow>
+					<PanelRow>
+							<TextControl
+								label={ _x(
+									'Pressable Link Text',
+									'inspector control label for the Pressable Link Text setting',
+									'wpcomsp-donations'
+								) }
+								value={ attributes.pressableLink.toString() }
+								onChange={ ( value ) => {
+									setAttributes( { pressableLink: value } );
+								} }
+								help={ __(
+									'Enter the text that should be shown on the Pressable link, leave empty to hide link.',
+									'wpcomsp-donations'
+								) }
+							/>
+						</PanelRow>
+						<PanelRow>
+							<TextControl
+								label={ _x(
+									'WordPress.com Link Text',
+									'inspector control label for the WordPress.com Link Text setting',
+									'wpcomsp-donations'
+								) }
+								value={ attributes.wpComLink.toString() }
+								onChange={ ( value ) => {
+									setAttributes( { wpComLink: value } );
+								} }
+								help={ __(
+									'Enter the text that should be shown on the WordPress.com link, leave empty to hide link.',
+									'wpcomsp-donations'
+								) }
+							/>
+						</PanelRow>
+						<PanelRow>
+							<TextControl
+								label={ _x(
+									'Separator',
+									'inspector control label for the separator setting',
+									'wpcomsp-donations'
+								) }
+								value={ attributes.separator.toString() }
+								onChange={ ( value ) => {
+									setAttributes( { separator: value } );
+								} }
+								help={ __(
+									'Please enter the separator character(s) to be used between the links. Plain text ONLY, no HTML.',
+									'wpcomsp-donations'
+								) }
+							/>
+						</PanelRow>
+						<PanelRow>
+							<CheckboxControl
+								label={ _x(
+									'Wrap links in a <span> tag',
+									'inspector control label for the wrap links setting',
+									'wpcomsp-donations')}
+								checked={ attributes.wrapLinks }
+								onChange={ ( value ) => {
+									setAttributes( { wrapLinks: value } );
+								} }
+								help={ __(
+									'Wrap the links in a <span> tag to allow for styling.',
+									'wpcomsp-donations'
+								) }
+							/>
+						</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+			<p {...useBlockProps()}>Hello from the editor!</p>
+		</Fragment>
 	);
 }
