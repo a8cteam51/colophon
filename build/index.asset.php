1f65132b80d611b051235353b802296014899aed
Tremi Dkhar <tremi@dkhar.com>
2025-05-01 14:24:26 +0530

    Generate build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '9e2f6c13ff86f54b1a55');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '3eead5c1aa4c3d8aaba2');
